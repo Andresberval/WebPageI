b96fdb5fc13e0e0c282dc0896ed9259b1699a7bf
Andrés Felipe <76921745+Andresberval@users.noreply.github.com>
2022-04-21 22:30:26 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.card-banners-one', {delay: 600});
-ScrollReveal().reveal('.info-cards', {delay: 600});
+ScrollReveal().reveal('.card-banners-one', {delay: 500});
+ScrollReveal().reveal('.info-cards', {delay: 500});
